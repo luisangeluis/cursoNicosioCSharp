cf88b031b2be9b463edf465b69e3750f4e4ab3e6
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2022-02-22 14:39:53 -0600

    Constructor en Herencia
    Ejemplo de como usar la herencia básica en c#.
    Ejemplo de constructor en herencia.

--- HerenciaBasica.cs
+++ HerenciaBasica.cs
@@ -7,12 +7,23 @@ class Program
         CEmpleado persona = new CEmpleado("luis",30,"operador",1000);    
         
         persona.MostrarEmpleado();
+        
+        Console.WriteLine("------------");
+        
+        CEmpleado dos = new CEmpleado("angel",29,"operador",2000);
+        dos.MostrarEmpleado();
+                Console.WriteLine("------------");
+
+        dos.PonerDatos("a",17,"nada",0);
+        dos.MostrarEmpleado();
+
     }
 }
 
 class CPersona{
     public string Nombre {set;get;}
     public int Edad{set;get;}
+    
     
     public CPersona(string pNombre,int pEdad){
         Nombre = pNombre;
@@ -28,8 +39,8 @@ class CEmpleado:CPersona{
     public string Puesto{set;get;}
     public double Sueldo{set;get;}
     
-    public CEmpleado(string pNombre,int pEdad,string pPuesto,double pSueldo):
-                    base(pNombre,pEdad)
+    public CEmpleado(string pNombre,int pEdad,string pPuesto,double pSueldo)
+        : base(pNombre,pEdad)
     {
         Puesto = pPuesto;
         Sueldo = pSueldo;
@@ -38,5 +49,19 @@ class CEmpleado:CPersona{
     public void MostrarEmpleado(){
         MostrarInfo();
         Console.WriteLine("puesto: {0}, sueldo {1}",Puesto,Sueldo);
+    }
+    
+    public void PonerDatos(string pNombre,int pEdad,string pPuesto,double pSueldo){
+       Nombre = pNombre;
+       Puesto = pPuesto;
+       Sueldo = pSueldo;
+       
+       if(pEdad < 18){
+            Edad =-1;
+
+       }else{
+           Edad = pEdad;
+       }
+       
     }
 }
